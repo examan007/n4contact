ac11649ce066cfecc8a59760bb95095879dfaeef
James Hayes <jhayes@corp.enqii.com>
2017-12-21 10:00:58 -0500

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
 import { AppComponent } from './app.component';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    FormsModule,
     BrowserModule
   ],
   providers: [],
